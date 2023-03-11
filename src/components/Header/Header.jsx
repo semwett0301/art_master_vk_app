3b95c656d3e763139c54d2dab5385833cc66a1d6
sem_wett <90400355+semwett0301@users.noreply.github.com>
2023-03-11 23:27:44 +0300

    Finish ApplicationPage.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,10 @@ import logo from './img/logo.svg'
 import cl from './Header.module.css'
 import {classJoiner} from "../../utils/classJoiner";
 
-const Header = ({ className }) => {
+const Header = ({ className, text }) => {
     return (
         <div className={classJoiner(cl.wrapper, className)}>
-            <div className={cl.text}>Заявка</div>
+            <div className={cl.text}>{text}</div>
             <img src={logo} alt={'Логотип'}/>
         </div>
     );
--- src/pages/ApplicationPage/ApplicationPage.jsx
+++ src/pages/ApplicationPage/ApplicationPage.jsx
@@ -10,70 +10,71 @@ import FilePicker from "../../components/UI/FilePicker/FilePicker";
 import icon from '../../components/UI/FilePicker/img/paper-clip.svg'
 
 const ApplicationPage = () => {
-    const [permission, setPermission] = useState(false)
+    const [dataPermission, setDataPermission] = useState(false)
+    const [codexPermission, setCodexPermission] = useState(false)
+
     const [selected, setSelected] = useState('мужской')
 
     return (
         <div className={cl.wrapper}>
-            <Header/>
-            <TextInput label={'Имя'} description={'Как в паспорте'} required={true} placeholder={'Например, Иван'}/>
-            <TextInput label={'Фамилия'} description={'Как в паспорте'} required={true}
-                       placeholder={'Например, Иванов'}/>
-            <TextInput label={'Дата рождения'} required={true} placeholder={'00.00.0000'}/>
-            <TextInput label={'Место проживания'} required={true} placeholder={'Начните вводить название'}/>
-            <TextInput label={'Телефон'} required={true} placeholder={'+7 (999) 999-99-99'}/>
-            <Checkbox content={[
-                {
-                    id: 1,
-                    type: 'text',
-                    text: 'Я согласен на обработку '
-                },
-                {
-                    id: 2,
-                    type: 'link',
-                    text: 'персональных данных',
-                    link: 'https://localhost:10888/'
-                }
-            ]} checked={permission} setChecked={setPermission}/>
-            <Checkbox content={[
-                {
-                    id: 1,
-                    type: 'text',
-                    text: 'Я ознакомился с '
-                },
-                {
-                    id: 2,
-                    type: 'link',
-                    text: 'Положением о Чемпионате',
-                    link: 'https://localhost:10888/'
-                },
-                {
-                    id: 3,
-                    type: 'text',
-                    text: ', Кодексом этики и '
-                },
-                {
-                    id: 4,
-                    type: 'link',
-                    text: 'принимаю условия',
-                    link: 'https://localhost:10888/'
-                },
-                {
-                    id: 5,
-                    type: 'text',
-                    text: ', необходимые для участия в Чемпионате'
-                }
-            ]} checked={permission} setChecked={setPermission}/>
-            <Radio required={true} label={'Пол'} content={[
-                'мужской',
-                'женский'
-            ]} selected={selected} setSelected={setSelected}/>
-            <TextField required={true} label={'Вопрос'} placeholder={'Длинное текстовое поле'}/>
-            <FilePicker className={cl.testWrapper} label={'Скан паспорта'} id={'scan_passport'} required={true} buttonLabel={'Загрузить'}
-                        instruction={'До 10 файлов (общий размер - до 20 Мб)'}
-                        description={'Или иные документы, согласно пунктам 2.4 Приложения №1 и №2 к Положению о Чемпионате. Допустимый формат: jpg, png, pdf'}
-                        imgSrc={icon}/>
-            {/*<Button placeholder={'Перейти к заполнению профиля'}/>*/}
+            <Header text={'Заявка'} className={cl.header}/>
+            <div className={cl.content}>
+                <div className={cl.contentBlock}>
+                    <TextInput required label={'Имя'} placeholder={'Например, Иван'} description={'Как в паспорте'} />
+                    <TextInput required label={'Фамилия'} placeholder={'Например, Иванов'} description={'Как в паспорте'} />
+                    <TextInput required label={'Дата рождения'} placeholder={'00.00.0000'} />
+                    <TextInput required label={'Компетенция'} placeholder={'не выбрано'} />
+                    <TextInput required label={'Гражданство'} placeholder={'не выбрано'} />
+                </div>
+                <div className={cl.contentBlock}>
+                    <TextInput required label={'Место проживания'} placeholder={'Начните вводить название'} />
+                    <TextInput required label={'Компетенция'} placeholder={'+7 (999) 999-99-99'} />
+                    <TextInput required label={'Гражданство'} placeholder={'info@artmasters.ru'} />
+                    <Checkbox content={[
+                        {
+                            id: 1,
+                            type: 'text',
+                            text: 'Я согласен на обработку '
+                        },
+                        {
+                            id: 2,
+                            type: 'link',
+                            text: 'персональных данных',
+                            link: 'https://localhost:10888/'
+                        }
+                    ]} checked={dataPermission} setChecked={setDataPermission}/>
+                    <Checkbox content={[
+                        {
+                            id: 1,
+                            type: 'text',
+                            text: 'Я ознакомился с '
+                        },
+                        {
+                            id: 2,
+                            type: 'link',
+                            text: 'Положением о Чемпионате',
+                            link: 'https://localhost:10888/'
+                        },
+                        {
+                            id: 3,
+                            type: 'text',
+                            text: ', Кодексом этики и '
+                        },
+                        {
+                            id: 4,
+                            type: 'link',
+                            text: 'принимаю условия',
+                            link: 'https://localhost:10888/'
+                        },
+                        {
+                            id: 5,
+                            type: 'text',
+                            text: ', необходимые для участия в Чемпионате'
+                        }
+                    ]} checked={codexPermission} setChecked={setCodexPermission}/>
+                </div>
+            </div>
+            <Button className={cl.button} placeholder={'Перейти к заполнению профиля'} />
         </div>
     );
 };
