f3e6aa3372973848efdf376cba3c44cc2b53229c
semen.mokrov <semen.mokrov@gmail.com>
2023-03-21 03:36:04 +0300

    Add web_designer

--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -15,7 +15,7 @@ const PortfolioPage = () => {
     const major = useSelector(state => state.major)
 
     const currentConfig = useMemo(() => {
-        return config.movie_operator
+        return config.web_designer
     }, [major])
 
     const {control, handleSubmit, formState, setError, watch} = useForm({
